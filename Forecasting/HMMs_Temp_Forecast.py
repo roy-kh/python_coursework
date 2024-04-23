6e4448e49afdbe8a7787fe28fd06cf1cfa067055
Roy Hayyat <126422418+roy-kh@users.noreply.github.com>
2024-04-22 18:08:06 -0700

    Update HMMs_Temp_Forecast.py

--- Forecasting/HMMs_Temp_Forecast.py
+++ Forecasting/HMMs_Temp_Forecast.py
@@ -4,9 +4,7 @@ import pandas as pd
 from hmmlearn import hmm
 import random
 
-
-''' Create a new file in which you will now train a Hidden Markov Model for the same task. Copy your code for problems
-1.1 to 1.5. No additional points for that. Make sure to import hmmlearn.'''
+'''This is part of a project where I am testing out a DNN forecasting model against the Hidden Markov Model'''
 
 # read the file
 df = pd.read_csv("temperature.csv")
@@ -24,17 +22,16 @@ plt.show()
 # Save only the temperature anomaly into a 2D numpy array
 anomaly_array = df['Anomaly'].values.reshape(-1, 1)
 
-# 15. Train a Gaussian Hidden Markov Model to fit the data. Parameters (number of hidden states and
+# Training a Gaussian Hidden Markov Model to fit the data. Parameters (number of hidden states and
 # number of iterations are your choice; choose for example 3 states and 100 iterations).
 model = hmm.GaussianHMM(n_components=3, n_iter=100, random_state=42)
 model.fit(anomaly_array)
 
-# 16. Predict the hidden states
+# Predicting the hidden states
 predict = model.predict(anomaly_array)
 print(predict)
 
-# 17. (4 points) Create a plot that colors every temperature anomaly (every month) in the color of its corresponding hidden state.
-# Background: the model assigned a hidden state to every anomaly with the .predict() command.
+# Creating a plot that colors every temperature anomaly (every month) in the color of its corresponding hidden state.
 states = pd.unique(predict)
 print("Unique states:", states)
 
@@ -48,8 +45,7 @@ plt.title("Temperature Anomalies Categorized by Hidden States")
 plt.grid(True)
 plt.show()
 
-# 18. (4 points) Visualize the state transition matrix using sns.heatmap.
-# You can access the probabilities using model.transmat_.
+# Visualizing the state transition matrix using sns.heatmap.
 plt.figure()
 sns.heatmap(model.transmat_, annot=True, cmap='viridis', fmt='.3f')
 plt.title("State Transition Matrix Heatmap")
